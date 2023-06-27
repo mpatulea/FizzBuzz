bffdb9875807b1243f03feaa91e902e032abd252
Maria Patulea <Maria.Patulea@softwire.com>
2023-06-27 10:35:35 +0300

    stretch goal 1 - user inputs number

--- index.ts
+++ index.ts
@@ -10,38 +10,50 @@ pairs.set(7, "Bang");
 // This is our main function
 function fizzbuzz(): void {
 
-    for (let i = 1; i < 256; i++) {
+    var readline = require('readline');
+    var rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
 
-        var ls: string[] = [];
+    rl.question("input your number: ", function(answer:string) {
+        var your_number = +answer;
 
-        pairs.forEach((key, code) => {
-            if ((code == 5 || code == 7) && is_divisible(i, code)) {
-                if (is_divisible(i, 13)) {
-                    ls.push("Fezz");
-                    ls.push(key);
-                } else {
+        for (let i = 1; i < your_number; i++) {
+
+            var ls: string[] = [];
+
+            pairs.forEach((key, code) => {
+                if ((code == 5 || code == 7) && is_divisible(i, code)) {
+                    if (is_divisible(i, 13)) {
+                        ls.push("Fezz");
+                        ls.push(key);
+                    } else {
+                        ls.push(key);
+                    }
+                } else if (is_divisible(i, code)) {
                     ls.push(key);
                 }
-            } else if (is_divisible(i, code)) {
-                ls.push(key);
+            });
+
+            if (is_divisible(i, 11) && !is_divisible(i, 13)) {
+                ls = ["Bong"];
+            } else if (is_divisible(i, 11) && is_divisible(i, 13)) {
+                ls = ["Fezz", "Bong"];
+            } else if (is_divisible(i, 17)) {
+                ls = ls.reverse();
             }
-        });
 
-        if (is_divisible(i, 11) && !is_divisible(i, 13)) {
-            ls = ["Bong"];
-        } else if (is_divisible(i, 11) && is_divisible(i, 13)) {
-            ls = ["Fezz", "Bong"];
-        } else if (is_divisible(i, 17)) {
-            ls = ls.reverse();
+            if (ls.length == 0) {
+                console.log(i);
+            } else {
+                console.log(ls.join(''));
+            }
+
         }
 
-        if (ls.length == 0) {
-            console.log(i);
-        } else {
-            console.log(ls.join(''));
-        }
-
-    }
+        rl.close();
+    });
 
 }
 
